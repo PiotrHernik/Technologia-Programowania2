eab50960b7f46e39b412a83e99e471adc540446f
PiotrHernik <piotr.hernik13@gmail.com>
2023-10-17 22:01:30 +0200

    Oby zadziałało

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        if(args.length < 1 || args.length > 2) {
+            System.out.println("Nieprawidlowa ilosc argumentow");
+            return;
+        }
+        Shape myShape;
+        if(args.length == 1){
+            myShape = new Circle(Double.parseDouble(args[0]));
+        } else {
+            myShape = new Rectangle((Double.parseDouble(args[0])), Double.parseDouble(args[1]));
+        }
+        System.out.println(myShape.area());
+    }
+}
